bf4a2d001e77ceb8178890a3bad021cd0102c4c6
Edilson Silva <eddysilva1994@gmail.com>
2025-09-14 00:35:53 -0300

    fix(auth/signup): existing user should raise exception

--- tests/application/usecases/auth/test_auth_signup_usecase.py
+++ tests/application/usecases/auth/test_auth_signup_usecase.py
@@ -5,6 +5,7 @@ import pytest
 from src.application.dtos.auth.auth_signup_dto import AuthSignupInputDTO
 from src.application.usecases.auth.auth_singup_usecase import AuthSignupUseCase
 from src.domain.entities.user_role import UserRole
+from src.domain.exceptions.user_exceptions import UserAlreadyExistsException
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.password_hasher import PasswordHasher
 
@@ -39,3 +40,23 @@ class TestAuthSignupUsecase:
         assert user.password == f'hashed_{signup_dto.password}'
         assert user.role == UserRole.ADMIN
         assert isinstance(user.created_at, datetime)
+
+    async def test_existing_user_should_raise_exception(
+        self,
+        fake_user_repository: UserRepository,
+        fake_password_hasher: PasswordHasher,
+    ):
+        signup_dto = AuthSignupInputDTO(
+            name='Test User',
+            email='test@example.com',
+            password='123456789',
+        )
+        signup_usecase = AuthSignupUseCase(
+            fake_user_repository, fake_password_hasher
+        )
+        await signup_usecase.execute(signup_dto)
+
+        with pytest.raises(UserAlreadyExistsException) as exc:
+            await signup_usecase.execute(signup_dto)
+
+        assert str(exc.value) == 'Email already registered'
