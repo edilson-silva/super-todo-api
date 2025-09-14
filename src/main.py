ea05c3a44eb41cace11da2c12f9ddc858ce85085
Edilson Silva <eddysilva1994@gmail.com>
2025-09-13 21:48:06 -0300

    chore: setup app to use /api/v1 as root path and redirect to /docs

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from .core.settings import settings
 from .infrastructure.db.session import Base, engine
@@ -18,6 +19,17 @@ async def lifespan(app: FastAPI):
     await engine.dispose()
 
 
-app = FastAPI(title=settings.APP_NAME, version='1.0.0', lifespan=lifespan)
+app = FastAPI(
+    title=settings.APP_NAME,
+    version='1.0.0',
+    lifespan=lifespan,
+    root_path='/api/v1',
+)
+
+
+@app.get('/')
+async def read_root():
+    return RedirectResponse(url='/docs')
+
 
 app.include_router(api_router)
