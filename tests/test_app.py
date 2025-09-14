588330693bea96d1d5b1069da7276ec1cb5e747c
Edilson Silva <eddysilva1994@gmail.com>
2025-09-13 23:32:21 -0300

    test(app): add app endpoints redirection tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,18 +2,28 @@ from fastapi import status
 from httpx import Client
 
 
-class AppTest:
-    def test_root_enpoint_should_return_doc(
+class TestApp:
+    def test_root_endpoint_should_redirect_to_docs_endpoint(
         self, client_with_mock_deps: Client
     ):
-        response = client_with_mock_deps.get('/')
+        response = client_with_mock_deps.get('/', follow_redirects=False)
+
+        assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+        assert response.headers['location'] == '/docs'
+
+        response = client_with_mock_deps.get('/docs')
 
         assert response.status_code == status.HTTP_200_OK
         assert 'SuperTodo - Swagger UI' in response.text
 
-    def test_api_v1_enpoint_should_return_doc(
+    def test_api_v1_endpoint_should_redirect_to_docs_endpoint(
         self, client_with_mock_deps: Client
     ):
+        response = client_with_mock_deps.get('/api/v1', follow_redirects=False)
+
+        assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
+        assert response.headers['location'] == 'http://testserver/api/v1/'
+
         response = client_with_mock_deps.get('/api/v1')
 
         assert response.status_code == status.HTTP_200_OK
@@ -22,7 +32,7 @@ class AppTest:
     def test_docs_enpoint_should_return_doc(
         self, client_with_mock_deps: Client
     ):
-        response = client_with_mock_deps.get('/api/v1')
+        response = client_with_mock_deps.get('/docs')
 
         assert response.status_code == status.HTTP_200_OK
         assert 'SuperTodo - Swagger UI' in response.text
