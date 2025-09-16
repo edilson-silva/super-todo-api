d35573a7bc13114d784c005574485fb8fa3458bd
Edilson Silva <eddysilva1994@gmail.com>
2025-09-16 01:04:16 -0300

    test(auth controller): signin with missing request params should return error

--- tests/presentation/api/v1/test_auth_controller.py
+++ tests/presentation/api/v1/test_auth_controller.py
@@ -68,3 +68,28 @@ class TestAuthSignupController:
 
         assert response2.status_code == status.HTTP_409_CONFLICT
         assert response2.json() == {'detail': 'Conflict'}
+
+
+class TestAuthSigninController:
+    def test_missing_request_params_should_return_error(
+        self, client_with_mock_deps: Client
+    ):
+        response = client_with_mock_deps.post('/auth/signin', json={})
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        assert response.json() == {
+            'detail': [
+                {
+                    'type': 'missing',
+                    'loc': ['body', 'email'],
+                    'msg': 'Field required',
+                    'input': {},
+                },
+                {
+                    'type': 'missing',
+                    'loc': ['body', 'password'],
+                    'msg': 'Field required',
+                    'input': {},
+                },
+            ]
+        }
