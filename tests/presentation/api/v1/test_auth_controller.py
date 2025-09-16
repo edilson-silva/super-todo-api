13bc6100c120c824040ac819cf2f70cb4c464439
Edilson Silva <eddysilva1994@gmail.com>
2025-09-16 01:10:10 -0300

    test(auth controller): signin with valid user credentials should return success with access token

--- tests/presentation/api/v1/test_auth_controller.py
+++ tests/presentation/api/v1/test_auth_controller.py
@@ -4,9 +4,17 @@ from httpx import Client
 
 
 @pytest.fixture(scope='class')
-def new_user_info():
+def signup_user_info():
     return {
         'name': 'Test User',
+        'email': 'test@example.com',
+        'password': '123456789',
+    }
+
+
+@pytest.fixture(scope='class')
+def signin_user_info():
+    return {
         'email': 'test@example.com',
         'password': '123456789',
     }
@@ -42,28 +50,28 @@ class TestAuthSignupController:
             ]
         }
 
-    def test_new_user_info_should_return_success(
-        self, client_with_mock_deps: Client, new_user_info: dict
+    def test_signup_user_info_should_return_success(
+        self, client_with_mock_deps: Client, signup_user_info: dict
     ):
         response = client_with_mock_deps.post(
-            '/auth/signup', json=new_user_info
+            '/auth/signup', json=signup_user_info
         )
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.text == 'null'
 
     def test_existing_user_info_should_return_conflict(
-        self, client_with_mock_deps: Client, new_user_info: dict
+        self, client_with_mock_deps: Client, signup_user_info: dict
     ):
         response1 = client_with_mock_deps.post(
-            '/auth/signup', json=new_user_info
+            '/auth/signup', json=signup_user_info
         )
 
         assert response1.status_code == status.HTTP_201_CREATED
         assert response1.text == 'null'
 
         response2 = client_with_mock_deps.post(
-            '/auth/signup', json=new_user_info
+            '/auth/signup', json=signup_user_info
         )
 
         assert response2.status_code == status.HTTP_409_CONFLICT
@@ -92,4 +100,25 @@ class TestAuthSigninController:
                     'input': {},
                 },
             ]
+        }
+
+    def test_valid_user_credentials_should_return_success_with_access_token(
+        self,
+        client_with_mock_deps: Client,
+        signup_user_info: dict,
+        signin_user_info: dict,
+    ):
+        signup_response = client_with_mock_deps.post(
+            '/auth/signup', json=signup_user_info
+        )
+
+        assert signup_response.status_code == status.HTTP_201_CREATED
+
+        signin_response = client_with_mock_deps.post(
+            '/auth/signin', json=signin_user_info
+        )
+
+        assert signin_response.status_code == status.HTTP_200_OK
+        assert signin_response.json() == {
+            'access_token': 'Bearer fake_token',
         }
