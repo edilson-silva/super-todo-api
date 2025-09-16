95806f1be1eea8107612eb7fbf1bcf03f99d347a
Edilson Silva <eddysilva1994@gmail.com>
2025-09-16 00:50:08 -0300

    test(auth controller): signup with invalid request params should return error

--- tests/presentation/api/v1/test_auth_controller.py
+++ tests/presentation/api/v1/test_auth_controller.py
@@ -9,6 +9,35 @@ class TestAuthController:
         'password': '123456789',
     }
 
+    def test_signup_with_invalid_request_params_should_return_error(
+        self, client_with_mock_deps: Client
+    ):
+        response = client_with_mock_deps.post('/auth/signup', json={})
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+        assert response.json() == {
+            'detail': [
+                {
+                    'type': 'missing',
+                    'loc': ['body', 'name'],
+                    'msg': 'Field required',
+                    'input': {},
+                },
+                {
+                    'type': 'missing',
+                    'loc': ['body', 'email'],
+                    'msg': 'Field required',
+                    'input': {},
+                },
+                {
+                    'type': 'missing',
+                    'loc': ['body', 'password'],
+                    'msg': 'Field required',
+                    'input': {},
+                },
+            ]
+        }
+
     def test_signup_with_new_user_info_should_return_success(
         self, client_with_mock_deps: Client
     ):
