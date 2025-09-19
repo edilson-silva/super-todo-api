c443e8a92c7cbfff4947c887bb112cdd9ae688a3
Edilson Silva <eddysilva1994@gmail.com>
2025-09-18 23:53:08 -0300

    test(user/delete): invalid id should raise exception

--- tests/application/usecases/user/test_user_delete_usecase.py
+++ tests/application/usecases/user/test_user_delete_usecase.py
@@ -3,13 +3,14 @@ import pytest
 from src.application.dtos.user.user_create_dto import UserCreateInputDTO
 from src.application.usecases.user.user_create_usecase import UserCreateUseCase
 from src.application.usecases.user.user_delete_usecase import UserDeleteUseCase
+from src.domain.exceptions.exceptions import NotFoundException
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.password_hasher import PasswordHasher
 
 
 @pytest.mark.asyncio
 class TestUserDeleteUsecase:
-    async def test_valid_id_should_delete(
+    async def test_valid_id_should_delete_and_return_success(
         self,
         fake_user_repository: UserRepository,
         fake_password_hasher: PasswordHasher,
@@ -30,3 +31,14 @@ class TestUserDeleteUsecase:
         user_deleted = await user_delete_usecase.execute(user_created.id)
 
         assert user_deleted is None
+
+    async def test_invalid_id_should_raise_exception(
+        self,
+        fake_user_repository: UserRepository,
+    ):
+        user_delete_usecase = UserDeleteUseCase(fake_user_repository)
+
+        with pytest.raises(NotFoundException) as exc:
+            await user_delete_usecase.execute('random-uuid')
+
+        assert str(exc.value) == 'Not found'
