134fb6af8b22941e9ec4d19c60f89b7a8d1de2aa
Edilson Silva <eddysilva1994@gmail.com>
2025-09-18 23:41:55 -0300

    test(user/update): should return a list with two users

--- tests/application/usecases/user/test_user_list_usecase.py
+++ tests/application/usecases/user/test_user_list_usecase.py
@@ -1,13 +1,19 @@
+from datetime import datetime
+
 import pytest
 
+from src.application.dtos.user.user_create_dto import UserCreateInputDTO
 from src.application.dtos.user.user_list_dto import UserListOutputDTO
+from src.application.usecases.user.user_create_usecase import UserCreateUseCase
 from src.application.usecases.user.user_list_usecase import UserListUseCase
+from src.domain.entities.user_role import UserRole
 from src.domain.repositories.user_repository import UserRepository
+from src.domain.security.password_hasher import PasswordHasher
 
 
 @pytest.mark.asyncio
 class TestUserListUsecase:
-    async def test_should_return_an_empty_list(
+    async def test_should_return_an_empty_users_list(
         self,
         fake_user_repository: UserRepository,
     ):
@@ -17,3 +23,52 @@ class TestUserListUsecase:
 
         assert isinstance(users, UserListOutputDTO)
         assert users.data == []
+
+    async def test_should_return_a_list_with_two_users(
+        self,
+        fake_user_repository: UserRepository,
+        fake_password_hasher: PasswordHasher,
+    ):
+        user_create_dto_1 = UserCreateInputDTO(
+            name='User1',
+            email='test1@example.com',
+            password='123456789',
+        )
+        user_create_dto_2 = UserCreateInputDTO(
+            name='User2',
+            email='test2@example.com',
+            password='123456789',
+        )
+
+        user_create_usecase = UserCreateUseCase(
+            fake_user_repository, fake_password_hasher
+        )
+
+        await user_create_usecase.execute(user_create_dto_1)
+        await user_create_usecase.execute(user_create_dto_2)
+        user_list_usecase = UserListUseCase(fake_user_repository)
+
+        users = await user_list_usecase.execute()
+
+        assert isinstance(users, UserListOutputDTO)
+        assert len(users.data) == 2
+
+        user_list_user_1 = users.data[0]
+
+        assert user_list_user_1.name == user_create_dto_1.name
+        assert isinstance(user_list_user_1.id, str)
+        assert user_list_user_1.id != ''
+        assert user_list_user_1.name == user_create_dto_1.name
+        assert user_list_user_1.email == user_create_dto_1.email
+        assert user_list_user_1.role == UserRole.ADMIN
+        assert isinstance(user_list_user_1.created_at, datetime)
+
+        user_list_user_2 = users.data[1]
+
+        assert user_list_user_2.name == user_create_dto_2.name
+        assert isinstance(user_list_user_2.id, str)
+        assert user_list_user_2.id != ''
+        assert user_list_user_2.name == user_create_dto_2.name
+        assert user_list_user_2.email == user_create_dto_2.email
+        assert user_list_user_2.role == UserRole.ADMIN
+        assert isinstance(user_list_user_2.created_at, datetime)
