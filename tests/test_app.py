16e77d2a93701920377abb04b5062aabf4f25ae4
Edilson Silva <eddysilva1994@gmail.com>
2025-09-23 00:20:01 -0300

    test(app): rewrite tests to use a new client with real database connection

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,38 +1,36 @@
 from fastapi import status
-from httpx import Client
+from httpx import AsyncClient
 
 
 class TestApp:
-    def test_root_endpoint_should_redirect_to_docs_endpoint(
-        self, client_with_mock_deps: Client
+    async def test_root_endpoint_should_redirect_to_docs_endpoint(
+        self, client: AsyncClient
     ):
-        response = client_with_mock_deps.get('/', follow_redirects=False)
+        response = await client.get('/', follow_redirects=False)
 
         assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
         assert response.headers['location'] == '/docs'
 
-        response = client_with_mock_deps.get('/docs')
+        response = await client.get('/docs')
 
         assert response.status_code == status.HTTP_200_OK
         assert 'SuperTodo - Swagger UI' in response.text
 
-    def test_api_v1_endpoint_should_redirect_to_docs_endpoint(
-        self, client_with_mock_deps: Client
+    async def test_api_v1_endpoint_should_redirect_to_docs_endpoint(
+        self, client: AsyncClient
     ):
-        response = client_with_mock_deps.get('/api/v1', follow_redirects=False)
+        response = await client.get('/api/v1', follow_redirects=False)
 
         assert response.status_code == status.HTTP_307_TEMPORARY_REDIRECT
-        assert response.headers['location'] == 'http://testserver/api/v1/'
+        assert response.headers['location'] == 'http://test/api/v1/'
 
-        response = client_with_mock_deps.get('/api/v1')
+        response = await client.get('/api/v1')
 
         assert response.status_code == status.HTTP_200_OK
         assert 'SuperTodo - Swagger UI' in response.text
 
-    def test_docs_enpoint_should_return_doc(
-        self, client_with_mock_deps: Client
-    ):
-        response = client_with_mock_deps.get('/docs')
+    async def test_docs_enpoint_should_return_doc(self, client: AsyncClient):
+        response = await client.get('/docs')
 
         assert response.status_code == status.HTTP_200_OK
         assert 'SuperTodo - Swagger UI' in response.text
