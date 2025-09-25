ee948918331b24ffe26a52be049a6c1897949e9b
Edilson Silva <eddysilva1994@gmail.com>
2025-09-25 00:31:28 -0300

    refactor(user): add correct type annotation for entity id

--- src/domain/entities/user_entity.py
+++ src/domain/entities/user_entity.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
 from typing import Optional
+from uuid import UUID
 
 from uuid_extensions import uuid7
 
@@ -13,7 +14,7 @@ class User:
     email: str
     password: str
     role: Optional[UserRole]
-    id: Optional[str] = field(default_factory=uuid7)
+    id: Optional[UUID | str | int | bytes] = field(default_factory=uuid7)
     avatar: Optional[str] = ''
     created_at: datetime = field(
         default_factory=lambda: datetime.now(timezone.utc)
