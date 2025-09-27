3fff35e0c6c2ee0b371931663be4f88ee1cd46c9
Edilson Silva <eddysilva1994@gmail.com>
2025-09-27 01:14:19 -0300

    test(security/password hasher): should check and return true for a valid password

--- tests/domain/security/test_password_hasher.py
+++ tests/domain/security/test_password_hasher.py
@@ -13,3 +13,15 @@ class TestPasswordHasher:
 
         assert isinstance(hashed_password, str)
         assert hashed_password != ''
+
+    async def test_should_check_and_return_true_for_a_valid_password(
+        self, password_hasher: PasswordHasher
+    ):
+        password = '123456789'
+        hashed_password = (
+            '$2b$10$zZvbpDt7Y2puEr50dK65x.LKRS63PrxL1L6YaW9p5ChgLOcBQrV9S'
+        )
+
+        valid = await password_hasher.async_check(password, hashed_password)
+
+        assert valid
