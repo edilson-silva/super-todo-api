bf526a7426838e46f2f6b7ea94e6839298ded9c9
Edilson Silva <eddysilva1994@gmail.com>
2025-09-27 01:12:00 -0300

    test(security/password hasher): should hash a password

--- tests/domain/security/test_password_hasher.py
+++ tests/domain/security/test_password_hasher.py
@@ -6,10 +6,10 @@ from src.domain.security.password_hasher import PasswordHasher
 @pytest.mark.asyncio
 class TestPasswordHasher:
     async def test_should_hash_a_password(
-        self, fake_password_hasher: PasswordHasher
+        self, password_hasher: PasswordHasher
     ):
-        password: str = '123456789'
-        hashed_password = await fake_password_hasher.async_hash(password)
+        password = '123456789'
+        hashed_password = await password_hasher.async_hash(password)
 
         assert isinstance(hashed_password, str)
-        assert hashed_password == f'hashed_{password}'
+        assert hashed_password != ''
