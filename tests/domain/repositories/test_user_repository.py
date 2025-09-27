096dd9bfec15f5b4b8a41d26b56fc2e338bbcd20
Edilson Silva <eddysilva1994@gmail.com>
2025-09-27 01:52:49 -0300

    test(user/repository): should find user by email

--- tests/domain/repositories/test_user_repository.py
+++ tests/domain/repositories/test_user_repository.py
@@ -54,6 +54,43 @@ class TestUserRepository:
         assert created_user.created_at == found_user.created_at
 
     @freeze_time(mock_datetime)
+    async def test_should_find_user_by_email(
+        self, user_repository: UserRepository
+    ):
+        user = User(
+            name='User 1',
+            email='user1@test.com',
+            password='123456789',
+            role=UserRole.ADMIN,
+        )
+
+        created_user = await user_repository.create(user)
+
+        assert isinstance(created_user.id, str)
+        assert created_user.id != ''
+        assert created_user.name == user.name
+        assert created_user.email == user.email
+        assert created_user.password == user.password
+        assert created_user.role == user.role
+        assert created_user.avatar == user.avatar
+        assert isinstance(created_user.created_at, datetime)
+        assert created_user.created_at == mock_datetime
+
+        found_user = await user_repository.find_by_email(
+            str(created_user.email)
+        )
+
+        assert found_user
+        assert isinstance(found_user.id, str)
+        assert created_user.id == found_user.id
+        assert created_user.name == found_user.name
+        assert created_user.email == found_user.email
+        assert created_user.password == found_user.password
+        assert created_user.role == found_user.role
+        assert created_user.avatar == found_user.avatar
+        assert created_user.created_at == found_user.created_at
+
+    @freeze_time(mock_datetime)
     async def test_should_list_users(self, user_repository: UserRepository):
         user_1 = User(
             name='User 1',
