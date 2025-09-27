ff26382d81f69cf9e460c694ef0a6695be7cfd2c
Edilson Silva <eddysilva1994@gmail.com>
2025-09-27 01:05:34 -0300

    test(user/list): replace fake dependencies to real rependencies fixtures

--- tests/application/usecases/user/test_user_list_usecase.py
+++ tests/application/usecases/user/test_user_list_usecase.py
@@ -15,19 +15,19 @@ from src.domain.security.password_hasher import PasswordHasher
 class TestUserListUsecase:
     async def test_should_return_an_empty_users_list(
         self,
-        fake_user_repository: UserRepository,
+        user_repository: UserRepository,
     ):
-        user_list_usecase = UserListUseCase(fake_user_repository)
+        user_list_usecase = UserListUseCase(user_repository)
 
-        users = await user_list_usecase.execute()
+        users = await user_list_usecase.execute(10, 0)
 
         assert isinstance(users, UserListOutputDTO)
         assert users.data == []
 
     async def test_should_return_a_list_with_two_users(
         self,
-        fake_user_repository: UserRepository,
-        fake_password_hasher: PasswordHasher,
+        user_repository: UserRepository,
+        password_hasher: PasswordHasher,
     ):
         user_create_dto_1 = UserCreateInputDTO(
             name='User1',
@@ -41,14 +41,14 @@ class TestUserListUsecase:
         )
 
         user_create_usecase = UserCreateUseCase(
-            fake_user_repository, fake_password_hasher
+            user_repository, password_hasher
         )
 
         await user_create_usecase.execute(user_create_dto_1)
         await user_create_usecase.execute(user_create_dto_2)
-        user_list_usecase = UserListUseCase(fake_user_repository)
+        user_list_usecase = UserListUseCase(user_repository)
 
-        users = await user_list_usecase.execute()
+        users = await user_list_usecase.execute(10, 0)
 
         assert isinstance(users, UserListOutputDTO)
         assert len(users.data) == 2
@@ -64,6 +64,45 @@ class TestUserListUsecase:
         assert isinstance(user_list_user_1.created_at, datetime)
 
         user_list_user_2 = users.data[1]
+
+        assert user_list_user_2.name == user_create_dto_2.name
+        assert isinstance(user_list_user_2.id, str)
+        assert user_list_user_2.id != ''
+        assert user_list_user_2.name == user_create_dto_2.name
+        assert user_list_user_2.email == user_create_dto_2.email
+        assert user_list_user_2.role == UserRole.ADMIN
+        assert isinstance(user_list_user_2.created_at, datetime)
+
+    async def test_should_return_a_list_with_one_user(
+        self,
+        user_repository: UserRepository,
+        password_hasher: PasswordHasher,
+    ):
+        user_create_dto_1 = UserCreateInputDTO(
+            name='User1',
+            email='test1@example.com',
+            password='123456789',
+        )
+        user_create_dto_2 = UserCreateInputDTO(
+            name='User2',
+            email='test2@example.com',
+            password='123456789',
+        )
+
+        user_create_usecase = UserCreateUseCase(
+            user_repository, password_hasher
+        )
+
+        await user_create_usecase.execute(user_create_dto_1)
+        await user_create_usecase.execute(user_create_dto_2)
+        user_list_usecase = UserListUseCase(user_repository)
+
+        users = await user_list_usecase.execute(10, 1)
+
+        assert isinstance(users, UserListOutputDTO)
+        assert len(users.data) == 1
+
+        user_list_user_2 = users.data[0]
 
         assert user_list_user_2.name == user_create_dto_2.name
         assert isinstance(user_list_user_2.id, str)
