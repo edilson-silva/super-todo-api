3a3735d8ee51cc85926ededb603064322086fd56
Edilson Silva <eddysilva1994@gmail.com>
2025-09-27 22:43:57 -0300

    feat(user): add updated_at entity property

--- src/domain/entities/user_entity.py
+++ src/domain/entities/user_entity.py
@@ -20,3 +20,6 @@ class User(BaseEntity):
     created_at: datetime = field(
         default_factory=lambda: datetime.now(timezone.utc)
     )
+    updated_at: datetime = field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
