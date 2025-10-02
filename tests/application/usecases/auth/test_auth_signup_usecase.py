731004cb99e9efc61734d25fba850b9ac6b9e635
Edilson Silva <eddysilva1994@gmail.com>
2025-10-02 00:15:10 -0300

    test(auth/signup): add new company name info

--- tests/application/usecases/auth/test_auth_signup_usecase.py
+++ tests/application/usecases/auth/test_auth_signup_usecase.py
@@ -6,6 +6,7 @@ from src.application.dtos.auth.auth_signup_dto import AuthSignupInputDTO
 from src.application.usecases.auth.auth_signup_usecase import AuthSignupUseCase
 from src.domain.entities.user_role import UserRole
 from src.domain.exceptions.user_exceptions import UserAlreadyExistsException
+from src.domain.repositories.company_repository import CompanyRepository
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.password_hasher import PasswordHasher
 
@@ -15,14 +16,18 @@ class TestAuthSignupUsecase:
     async def test_new_user_info_should_return_created_user(
         self,
         user_repository: UserRepository,
+        company_repository: CompanyRepository,
         password_hasher: PasswordHasher,
     ):
         signup_dto = AuthSignupInputDTO(
+            company_name='test company',
             name='Test User',
             email='test@example.com',
             password='123456789',
         )
-        signup_usecase = AuthSignupUseCase(user_repository, password_hasher)
+        signup_usecase = AuthSignupUseCase(
+            user_repository, company_repository, password_hasher
+        )
 
         response = await signup_usecase.execute(signup_dto)
 
@@ -42,14 +47,18 @@ class TestAuthSignupUsecase:
     async def test_existing_user_should_raise_exception(
         self,
         user_repository: UserRepository,
+        company_repository: CompanyRepository,
         password_hasher: PasswordHasher,
     ):
         signup_dto = AuthSignupInputDTO(
+            company_name='test company',
             name='Test User',
             email='test@example.com',
             password='123456789',
         )
-        signup_usecase = AuthSignupUseCase(user_repository, password_hasher)
+        signup_usecase = AuthSignupUseCase(
+            user_repository, company_repository, password_hasher
+        )
         await signup_usecase.execute(signup_dto)
 
         with pytest.raises(UserAlreadyExistsException) as exc:
