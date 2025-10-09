05f86bc308d78b12667dee6060d8887e18e2e8ef
Edilson Silva <eddysilva1994@gmail.com>
2025-10-09 00:41:28 -0300

    feat(user/list): allow action only for admin users

--- src/application/usecases/user/user_list_usecase.py
+++ src/application/usecases/user/user_list_usecase.py
@@ -1,5 +1,8 @@
 from src.application.dtos.user.user_list_dto import UserListOutputDTO
 from src.application.dtos.user.user_output_dto import UserOutputDTO
+from src.domain.entities.user_entity import User
+from src.domain.entities.user_role import UserRole
+from src.domain.exceptions.auth_exceptions import UnauthorizedException
 from src.domain.repositories.user_repository import UserRepository
 
 
@@ -11,18 +14,23 @@ class UserListUseCase:
         self.repository = repository
 
     async def execute(
-        self, company_id: str, limit: int, offset: int
+        self, requester: User, limit: int, offset: int
     ) -> UserListOutputDTO:
         """
         Get the list of users.
 
-        :param company_id: The company id to filter users.
+        :requester: User trying to perform the action (must be an admin).
         :param limit: Maximum number of users returned.
         :param offset: Number of users ignored in the search.
 
         :return: List of users.
         """
-        users = await self.repository.find_all(company_id, limit, offset)
+        if requester.role != UserRole.ADMIN:
+            raise UnauthorizedException()
+
+        users = await self.repository.find_all(
+            requester.company_id, limit, offset
+        )
 
         users_output_dto = UserListOutputDTO(
             data=list(map(lambda u: UserOutputDTO.model_validate(u), users))
