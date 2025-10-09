2a1004917a3a1936411294e8256e080c3351c106
Edilson Silva <eddysilva1994@gmail.com>
2025-10-08 23:41:17 -0300

    fix(security): handle token decode errors on pyjwt implementation

--- src/infrastructure/security/token_generator_pyjwt.py
+++ src/infrastructure/security/token_generator_pyjwt.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta, timezone
 
 from jwt import decode, encode
+from jwt.exceptions import (
+    DecodeError,
+    ExpiredSignatureError,
+    InvalidTokenError,
+)
 
 from src.application.dtos.security.token_generator_decode_dto import (
     TokenGeneratorDecodeOutputDTO,
@@ -11,7 +16,6 @@ from src.application.dtos.security.token_generator_encode_dto import (
 )
 from src.core.settings import settings
 from src.domain.entities.user_role import UserRole
-from src.domain.exceptions.auth_exceptions import InvalidTokenException
 from src.domain.security.token_generator import TokenGenerator
 
 
@@ -71,5 +75,5 @@ class TokenGeneratorPyJWT(TokenGenerator):
                 user_role=UserRole(decoded['role']),
                 company_id=decoded['company'],
             )
-        except InvalidTokenException:
+        except (ExpiredSignatureError, DecodeError, InvalidTokenError):
             return None
