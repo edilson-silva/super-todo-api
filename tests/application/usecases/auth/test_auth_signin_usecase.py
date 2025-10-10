175f41dc436d8341011adb556240bfe516a99671
Edilson Silva <eddysilva1994@gmail.com>
2025-10-10 19:35:42 -0300

    test(auth/signin): update tests to use new fixtures

--- tests/application/usecases/auth/test_auth_signin_usecase.py
+++ tests/application/usecases/auth/test_auth_signin_usecase.py
@@ -4,10 +4,9 @@ from src.application.dtos.auth.auth_signin_dto import (
     AuthSigninInputDTO,
     AuthSigninOutputDTO,
 )
-from src.application.dtos.auth.auth_signup_dto import AuthSignupInputDTO
 from src.application.usecases.auth.auth_signin_usecase import AuthSigninUseCase
-from src.application.usecases.auth.auth_signup_usecase import AuthSignupUseCase
 from src.core.settings import settings
+from src.domain.entities.user_entity import User
 from src.domain.exceptions.auth_exceptions import InvalidCredentialsException
 from src.domain.exceptions.exceptions import NotFoundException
 from src.domain.repositories.company_repository import CompanyRepository
@@ -24,25 +23,12 @@ class TestAuthSigninUsecase:
         company_repository: CompanyRepository,
         password_hasher: PasswordHasher,
         token_generator: TokenGenerator,
+        admin_user: User,
+        admin_user_info: dict,
     ):
-        name = 'test'
-        email = 'test@example.com'
-        password = '123456789'
-
-        signup_dto = AuthSignupInputDTO(
-            company_name='test company',
-            name=name,
-            email=email,
-            password=password,
-        )
-        signup_usecase = AuthSignupUseCase(
-            user_repository, company_repository, password_hasher
-        )
-        await signup_usecase.execute(data=signup_dto)
-
         signin_dto = AuthSigninInputDTO(
-            email=email,
-            password=password,
+            email=admin_user_info['email'],
+            password=admin_user_info['password'],
         )
         signin_usecase = AuthSigninUseCase(
             user_repository, password_hasher, token_generator
@@ -62,27 +48,12 @@ class TestAuthSigninUsecase:
         company_repository: CompanyRepository,
         password_hasher: PasswordHasher,
         token_generator: TokenGenerator,
+        admin_user: User,
+        admin_user_info: dict,
     ):
-        company_name = 'test company'
-        name = 'test'
-        email = 'test@example.com'
-        signup_password = '123456789'
-        signin_password = '1234567890'
-
-        signup_dto = AuthSignupInputDTO(
-            company_name=company_name,
-            name=name,
-            email=email,
-            password=signup_password,
-        )
-        signup_usecase = AuthSignupUseCase(
-            user_repository, company_repository, password_hasher
-        )
-        await signup_usecase.execute(data=signup_dto)
-
         signin_dto = AuthSigninInputDTO(
-            email=email,
-            password=signin_password,
+            email=admin_user_info['email'],
+            password='{}_err'.format(admin_user_info['password']),
         )
         signin_usecase = AuthSigninUseCase(
             user_repository, password_hasher, token_generator
@@ -93,24 +64,23 @@ class TestAuthSigninUsecase:
 
         assert str(exc.value) == 'Invalid credentials'
 
-    async def test_non_existing_user_should_raise_exception(
+    async def test_not_found_user_should_raise_exception(
         self,
         user_repository: UserRepository,
         password_hasher: PasswordHasher,
         token_generator: TokenGenerator,
+        admin_user_info: dict,
     ):
-        email = 'test@example.com'
-        password = '123456789'
-
-        signup_dto = AuthSigninInputDTO(
-            email=email,
-            password=password,
+        signin_dto = AuthSigninInputDTO(
+            email=admin_user_info['email'],
+            password='{}_err'.format(admin_user_info['password']),
         )
+
         signin_usecase = AuthSigninUseCase(
             user_repository, password_hasher, token_generator
         )
 
         with pytest.raises(NotFoundException) as exc:
-            await signin_usecase.execute(signup_dto)
+            await signin_usecase.execute(signin_dto)
 
         assert str(exc.value) == 'Not found'
