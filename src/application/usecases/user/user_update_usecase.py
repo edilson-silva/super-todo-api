ac52799f15c400f0350cdd1e58afef48c3d7b271
Edilson Silva <eddysilva1994@gmail.com>
2025-10-10 00:25:14 -0300

    feat(user/update): allow action only for same company users

--- src/application/usecases/user/user_update_usecase.py
+++ src/application/usecases/user/user_update_usecase.py
@@ -5,6 +5,8 @@ from src.application.dtos.user.user_update_dto import (
     UserUpdateOutputDTO,
 )
 from src.domain.entities.user_entity import User
+from src.domain.entities.user_role import UserRole
+from src.domain.exceptions.auth_exceptions import UnauthorizedException
 from src.domain.exceptions.exceptions import NotFoundException
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.password_hasher import PasswordHasher
@@ -22,21 +24,28 @@ class UserUpdateUseCase:
         self.password_hasher = password_hasher
 
     async def execute(
-        self, user_id: str, company_id: str, data: UserUpdateInputDTO
+        self, requester: User, user_id: str, data: UserUpdateInputDTO
     ) -> UserUpdateOutputDTO:
         """
         Update an user info based on its id.
 
-        :param id: Id of user to update
-        :company_id: Id of the company the user belongs to.
+        :param requester:
+            User trying to perform the action
+            (must be an admin or the own user).
+        :param user_id: Id of user to update
         :param data: User new data.
 
         :return: The updated User entity.
         """
-        user = await self.repository.find_by_id(user_id, company_id)
+        user = await self.repository.find_by_id(user_id, requester.company_id)
 
         if not user:
             raise NotFoundException()
+
+        if user.role != UserRole.ADMIN and str(user.id) != str(requester.id):
+            raise UnauthorizedException(
+                "You don't have enough permission to perform this action."
+            )
 
         hashed_password = await self.password_hasher.async_hash(data.password)
 
