7ba8e96c71864164e7fb65ac53d52002d382d0b1
Edilson Silva <eddysilva1994@gmail.com>
2025-10-11 00:49:36 -0300

    test(user/delete): non admin requester should raise exception

--- tests/application/usecases/user/test_user_delete_usecase.py
+++ tests/application/usecases/user/test_user_delete_usecase.py
@@ -5,6 +5,7 @@ from uuid_extensions import uuid7str
 
 from src.application.usecases.user.user_delete_usecase import UserDeleteUseCase
 from src.domain.entities.user_entity import User
+from src.domain.exceptions.auth_exceptions import UnauthorizedException
 from src.domain.exceptions.exceptions import NotFoundException
 from src.domain.repositories.user_repository import UserRepository
 
@@ -34,3 +35,12 @@ class TestUserDeleteUsecase:
             await self.usecase.execute(requester, uuid7str())
 
         assert str(exc.value) == 'Not found'
+
+    async def test_non_admin_requester_should_raise_exception(self):
+        requester = self.users[1]
+        user_id = str(self.users[2].id)
+
+        with pytest.raises(UnauthorizedException) as exc:
+            await self.usecase.execute(requester, user_id)
+
+        assert str(exc.value) == 'Unauthorized'
