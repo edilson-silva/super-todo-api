39e314e4d20e679108b7aecab088d6c90e1c9062
Edilson Silva <eddysilva1994@gmail.com>
2025-10-11 00:48:06 -0300

    test(user/delete): rewrite tests generalizing fixtures using a setup method

--- tests/application/usecases/user/test_user_delete_usecase.py
+++ tests/application/usecases/user/test_user_delete_usecase.py
@@ -7,41 +7,30 @@ from src.application.usecases.user.user_delete_usecase import UserDeleteUseCase
 from src.domain.entities.user_entity import User
 from src.domain.exceptions.exceptions import NotFoundException
 from src.domain.repositories.user_repository import UserRepository
-from src.domain.security.password_hasher import PasswordHasher
+
+
+@pytest.fixture(autouse=True)
+def setup(
+    request, admin_company_users: List[User], user_repository: UserRepository
+):
+    request.cls.users = admin_company_users
+    request.cls.usecase = UserDeleteUseCase(user_repository)
 
 
 @pytest.mark.asyncio
 class TestUserDeleteUsecase:
-    async def test_found_user_should_be_deleted_and_return_success(
-        self,
-        user_repository: UserRepository,
-        password_hasher: PasswordHasher,
-        admin_company_users: List[User],
-        admin_user_info: dict,
-    ):
-        requester = admin_company_users[0]
-        user_id = str(admin_company_users[1].id)
+    async def test_found_user_should_be_deleted_and_return_success(self):
+        requester = self.users[0]
+        user_id = str(self.users[1].id)
 
-        user_delete_usecase = UserDeleteUseCase(user_repository)
-
-        user_deleted = await user_delete_usecase.execute(
-            requester,
-            user_id,
-        )
+        user_deleted = await self.usecase.execute(requester, user_id)
 
         assert user_deleted is None
 
-    async def test_not_found_user_should_raise_exception(
-        self,
-        user_repository: UserRepository,
-        admin_user: User,
-    ):
-        user_delete_usecase = UserDeleteUseCase(user_repository)
+    async def test_not_found_user_should_raise_exception(self):
+        requester = self.users[0]
 
         with pytest.raises(NotFoundException) as exc:
-            await user_delete_usecase.execute(
-                admin_user,
-                uuid7str(),
-            )
+            await self.usecase.execute(requester, uuid7str())
 
         assert str(exc.value) == 'Not found'
