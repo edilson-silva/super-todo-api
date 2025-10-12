13a1ed06ed51beb3f31799a9309f8deaf7eb9259
Edilson Silva <eddysilva1994@gmail.com>
2025-10-12 20:41:23 -0300

    test(auth/signin): rewrite tests generalizing fixtures using a setup method

--- tests/application/usecases/auth/test_auth_signin_usecase.py
+++ tests/application/usecases/auth/test_auth_signin_usecase.py
@@ -1,3 +1,5 @@
+from typing import Dict, List, Tuple
+
 import pytest
 
 from src.application.dtos.auth.auth_signin_dto import (
@@ -9,32 +11,43 @@ from src.core.settings import settings
 from src.domain.entities.user_entity import User
 from src.domain.exceptions.auth_exceptions import InvalidCredentialsException
 from src.domain.exceptions.exceptions import NotFoundException
-from src.domain.repositories.company_repository import CompanyRepository
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.password_hasher import PasswordHasher
 from src.domain.security.token_generator import TokenGenerator
 
+SetupType = Tuple[Dict, AuthSigninUseCase]
+
 
 @pytest.mark.asyncio
 class TestAuthSigninUsecase:
-    async def test_valid_credentials_should_return_access_token(
+    @pytest.fixture
+    def setup(
         self,
+        admin_company_users: List[User],
         user_repository: UserRepository,
-        company_repository: CompanyRepository,
         password_hasher: PasswordHasher,
         token_generator: TokenGenerator,
-        admin_user: User,
         admin_user_info: dict,
-    ):
-        signin_dto = AuthSigninInputDTO(
-            email=admin_user_info['email'],
-            password=admin_user_info['password'],
-        )
-        signin_usecase = AuthSigninUseCase(
+    ) -> SetupType:
+        requester = admin_user_info
+        usecase = AuthSigninUseCase(
             user_repository, password_hasher, token_generator
         )
+        return requester, usecase
 
-        response = await signin_usecase.execute(signin_dto)
+    async def test_valid_credentials_should_return_access_token(
+        self,
+        setup: SetupType,
+        admin_user_info: dict,
+    ):
+        requester, usecase = setup
+
+        signin_dto = AuthSigninInputDTO(
+            email=requester['email'],
+            password=requester['password'],
+        )
+
+        response = await usecase.execute(signin_dto)
 
         assert isinstance(response, AuthSigninOutputDTO)
         assert isinstance(response.access_token, str)
@@ -44,43 +57,32 @@ class TestAuthSigninUsecase:
 
     async def test_invalid_credentials_should_raise_exception(
         self,
-        user_repository: UserRepository,
-        company_repository: CompanyRepository,
-        password_hasher: PasswordHasher,
-        token_generator: TokenGenerator,
-        admin_user: User,
-        admin_user_info: dict,
+        setup: SetupType,
     ):
+        requester, usecase = setup
+
         signin_dto = AuthSigninInputDTO(
-            email=admin_user_info['email'],
-            password='{}_err'.format(admin_user_info['password']),
-        )
-        signin_usecase = AuthSigninUseCase(
-            user_repository, password_hasher, token_generator
+            email=requester['email'],
+            password='{}_err'.format(requester['password']),
         )
 
         with pytest.raises(InvalidCredentialsException) as exc:
-            await signin_usecase.execute(signin_dto)
+            await usecase.execute(signin_dto)
 
         assert str(exc.value) == 'Invalid credentials'
 
     async def test_not_found_user_should_raise_exception(
         self,
-        user_repository: UserRepository,
-        password_hasher: PasswordHasher,
-        token_generator: TokenGenerator,
-        admin_user_info: dict,
+        setup: SetupType,
     ):
-        signin_dto = AuthSigninInputDTO(
-            email=admin_user_info['email'],
-            password='{}_err'.format(admin_user_info['password']),
-        )
+        requester, usecase = setup
 
-        signin_usecase = AuthSigninUseCase(
-            user_repository, password_hasher, token_generator
+        signin_dto = AuthSigninInputDTO(
+            email='invalid_{}'.format(requester['email']),
+            password='{}_err'.format(requester['password']),
         )
 
         with pytest.raises(NotFoundException) as exc:
-            await signin_usecase.execute(signin_dto)
+            await usecase.execute(signin_dto)
 
         assert str(exc.value) == 'Not found'
