6acbf1fbffc712983193900745ce171eb21e8f67
Edilson Silva <eddysilva1994@gmail.com>
2025-10-11 22:41:55 -0300

    test(user/update): rewrite tests generalizing fixtures using a setup method

--- tests/application/usecases/user/test_user_update_usecase.py
+++ tests/application/usecases/user/test_user_update_usecase.py
@@ -1,31 +1,23 @@
 from datetime import datetime, timezone
+from typing import List, Tuple
 
 import pytest
 from freezegun import freeze_time
 from uuid_extensions import uuid7str
 
-from src.application.dtos.user.user_create_dto import UserCreateInputDTO
 from src.application.dtos.user.user_update_dto import (
     UserUpdateInputDTO,
     UserUpdateOutputDTO,
 )
-from src.application.usecases.user.user_create_usecase import UserCreateUseCase
 from src.application.usecases.user.user_update_usecase import UserUpdateUseCase
+from src.domain.entities.user_entity import User
 from src.domain.entities.user_role import UserRole
 from src.domain.exceptions.exceptions import NotFoundException
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.password_hasher import PasswordHasher
 
-mock_create_datetime = datetime(
-    2025,
-    1,
-    1,
-    0,
-    0,
-    0,
-    0,
-    timezone.utc,
-)
+SetupType = Tuple[List[User], UserUpdateUseCase]
+
 mock_update_datetime = datetime(
     2025,
     1,
@@ -40,70 +32,59 @@ mock_update_datetime = datetime(
 
 @pytest.mark.asyncio
 class TestUserUpdateUsecase:
-    company_id = uuid7str()
-
-    async def test_valid_id_should_return_updated_user_info(
+    @pytest.fixture
+    def setup(
         self,
+        admin_company_users: List[User],
         user_repository: UserRepository,
         password_hasher: PasswordHasher,
-    ):
-        user_create_dto = UserCreateInputDTO(
-            name='Test User',
-            email='test@example.com',
-            password='123456789',
-        )
-        user_create_usecase = UserCreateUseCase(
+    ) -> SetupType:
+        return admin_company_users, UserUpdateUseCase(
             user_repository, password_hasher
         )
 
-        with freeze_time(mock_create_datetime):
-            user_created = await user_create_usecase.execute(
-                self.company_id, user_create_dto
-            )
+    async def test_valid_id_should_return_updated_user_info(
+        self, setup: SetupType
+    ):
+        users, usecase = setup
+        requester = users[0]
+        user_expected = users[1]
+        user_expected_id = str(user_expected.id)
 
         user_update_dto = UserUpdateInputDTO(
             name='Updated Name',
             password='updated_pass',
             role=UserRole.USER,
             avatar='updated_avatar',
-        )
-        user_update_usecase = UserUpdateUseCase(
-            user_repository, password_hasher
         )
 
         with freeze_time(mock_update_datetime):
-            user_updated = await user_update_usecase.execute(
-                user_created.id, self.company_id, user_update_dto
+            user_updated = await usecase.execute(
+                requester, user_expected_id, user_update_dto
             )
 
         assert isinstance(user_updated, UserUpdateOutputDTO)
-        assert user_updated.id == user_created.id
+        assert user_updated.id == user_expected.id
         assert user_updated.name == user_update_dto.name
-        assert user_updated.email == user_created.email
+        assert user_updated.email == user_expected.email
         assert user_updated.avatar == user_update_dto.avatar
         assert user_updated.role == user_update_dto.role
-        assert user_updated.created_at == mock_create_datetime
+        assert user_updated.created_at == user_expected.created_at
         assert isinstance(user_updated.created_at, datetime)
         assert user_updated.updated_at == mock_update_datetime
 
-    async def test_invalid_id_should_raise_exception(
-        self,
-        user_repository: UserRepository,
-        password_hasher: PasswordHasher,
-    ):
+    async def test_invalid_id_should_raise_exception(self, setup: SetupType):
+        users, usecase = setup
+        requester = users[0]
+
         user_update_dto = UserUpdateInputDTO(
             name='Updated Name',
             password='updated_pass',
             role=UserRole.USER,
             avatar='updated_avatar',
         )
-        user_update_usecase = UserUpdateUseCase(
-            user_repository, password_hasher
-        )
 
         with pytest.raises(NotFoundException) as exc:
-            await user_update_usecase.execute(
-                uuid7str(), self.company_id, user_update_dto
-            )
+            await usecase.execute(requester, uuid7str(), user_update_dto)
 
         assert str(exc.value) == 'Not found'
