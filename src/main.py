2eaf6b719e1d86d890395719a17b65d4b5de4417
Edilson Silva <eddysilva1994@gmail.com>
2025-10-18 00:57:08 -0300

    refactor: remove unused lifespan

--- src/main.py
+++ src/main.py
@@ -1,32 +1,15 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI, status
 from starlette.responses import RedirectResponse
 
+from src.core.settings import settings
+from src.presentation.api.router import api_router
 from src.presentation.api.v1.security.exceptions_handler import (
     http_exception_handler,
 )
 
-from .core.settings import settings
-from .infrastructure.db.session import Base, engine
-from .presentation.api.router import api_router
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-
-    # Shutdown (engine cleanup)
-    await engine.dispose()
-
-
 app = FastAPI(
     title=settings.APP_NAME,
     version='1.0.0',
-    lifespan=lifespan,
     root_path='/api/v1',
 )
 http_exception_handler(app)
