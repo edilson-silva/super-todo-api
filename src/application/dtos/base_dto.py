9e77cf6f4e99a42debf0517f8e10f355264acf48
Edilson Silva <eddysilva1994@gmail.com>
2025-10-18 19:32:55 -0300

    refactor: remove unecessary model validation for uuid and dates

--- src/application/dtos/base_dto.py
+++ src/application/dtos/base_dto.py
@@ -1,6 +1,3 @@
-from datetime import datetime, timezone
-from uuid import UUID
-
 from pydantic import BaseModel, ConfigDict
 
 
@@ -8,17 +5,3 @@ class BaseDTO(BaseModel):
     model_config = ConfigDict(
         from_attributes=True,  # allow building from dataclasses/ORM
     )
-
-    @classmethod
-    def model_validate(cls, obj):
-        for attr, value in vars(obj).items():
-            # Convert all UUID properties to strings
-            if isinstance(value, UUID):
-                setattr(obj, attr, str(value))
-
-            # Set all datetime properties timezone to utc
-            if isinstance(value, datetime):
-                if value.tzinfo is None:
-                    setattr(obj, attr, value.replace(tzinfo=timezone.utc))
-
-        return super().model_validate(obj)
