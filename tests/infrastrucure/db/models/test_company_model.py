a24bfe4189a405ea2733c84166450d82b78bc45d
Edilson Silva <eddysilva1994@gmail.com>
2025-10-18 19:44:50 -0300

    test(db/company): invalid max users should raise value error

--- tests/infrastrucure/db/models/test_company_model.py
+++ tests/infrastrucure/db/models/test_company_model.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 
+import pytest
 from uuid_extensions import uuid7str
 
 from src.domain.entities.company_type import CompanyType
@@ -18,9 +19,7 @@ mock_datetime = datetime(
 
 
 class TestCompanyModel:
-    def test_valid_info_should_create_a_model(
-        self,
-    ):
+    def test_valid_info_should_create_a_model(self):
         company_info = {
             'id': uuid7str(),
             'name': 'Test Company',
@@ -38,3 +37,20 @@ class TestCompanyModel:
         assert model.max_users == company_info['max_users']
         assert model.created_at == company_info['created_at']
         assert model.updated_at == company_info['updated_at']
+
+    def test_invalid_max_users_should_raise_value_error(self):
+        company_info = {
+            'id': uuid7str(),
+            'name': 'Test Company',
+            'type': CompanyType.PRO,
+            'max_users': 0,
+            'created_at': mock_datetime,
+            'updated_at': mock_datetime,
+        }
+
+        with pytest.raises(ValueError) as exc_info:
+            CompanyModel(**company_info)
+
+        assert str(exc_info.value) == (
+            'Max users value must be greater than 0, but got 0'
+        )
