d1906da4066aba9b030ec6269b0297f7fc76e7b4
Edilson Silva <eddysilva1994@gmail.com>
2025-10-19 01:23:13 -0300

    test(security): empty token should raise invalid token exception on token hanler

---
+++ tests/presentation/api/v1/security/test_token_handler.py
@@ -0,0 +1,27 @@
+import pytest
+
+from src.application.dtos.security.token_generator_encode_dto import (
+    TokenGeneratorEncodeOutputDTO,
+)
+from src.domain.exceptions.auth_exceptions import InvalidTokenException
+from src.domain.repositories.user_repository import UserRepository
+from src.domain.security.token_generator import TokenGenerator
+from src.presentation.api.v1.security.token_handler import (
+    get_requester_from_token,
+)
+
+
+@pytest.mark.asyncio
+class TestGetRequesterFromToken:
+    async def test_empty_token_should_raise_invalid_token_exception(
+        self,
+        empty_token: TokenGeneratorEncodeOutputDTO,
+        token_generator: TokenGenerator,
+        user_repository: UserRepository,
+    ):
+        with pytest.raises(InvalidTokenException) as exc_info:
+            await get_requester_from_token(
+                empty_token.access_token, token_generator, user_repository
+            )
+
+        assert str(exc_info.value) == 'Invalid token'
