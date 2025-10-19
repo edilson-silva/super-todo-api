65492c850c2625d036b1f51655e6e2a0203957d4
Edilson Silva <eddysilva1994@gmail.com>
2025-10-19 01:25:47 -0300

    test(security): invalid user token should raise invalid token exception on token handler

--- tests/presentation/api/v1/security/test_token_handler.py
+++ tests/presentation/api/v1/security/test_token_handler.py
@@ -3,7 +3,10 @@ import pytest
 from src.application.dtos.security.token_generator_encode_dto import (
     TokenGeneratorEncodeOutputDTO,
 )
-from src.domain.exceptions.auth_exceptions import InvalidTokenException
+from src.domain.exceptions.auth_exceptions import (
+    InvalidTokenException,
+    UnauthorizedException,
+)
 from src.domain.repositories.user_repository import UserRepository
 from src.domain.security.token_generator import TokenGenerator
 from src.presentation.api.v1.security.token_handler import (
@@ -50,3 +53,18 @@ class TestGetRequesterFromToken:
             )
 
         assert str(exc_info.value) == 'Invalid token'
+
+    async def test_invalid_user_token_should_raise_invalid_token_exception(
+        self,
+        invalid_user_token: TokenGeneratorEncodeOutputDTO,
+        token_generator: TokenGenerator,
+        user_repository: UserRepository,
+    ):
+        with pytest.raises(UnauthorizedException) as exc_info:
+            await get_requester_from_token(
+                invalid_user_token.access_token,
+                token_generator,
+                user_repository,
+            )
+
+        assert str(exc_info.value) == 'Unauthorized'
