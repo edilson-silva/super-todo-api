222a4f514699aa05e462d8d2157f0fdb8d1fc2c6
Edilson Silva <eddysilva1994@gmail.com>
2025-10-19 00:21:13 -0300

    test(user/repository): find by valid id should return user

--- tests/infrastrucure/repositories/test_user_repository_sqlalchemy.py
+++ tests/infrastrucure/repositories/test_user_repository_sqlalchemy.py
@@ -56,3 +56,23 @@ class TestUserRepositorySQLAlchemy:
         )
 
         assert found_user is None
+
+    async def test_find_by_valid_id_should_return_user(
+        self,
+        user_repository: UserRepository,
+        admin_user: User,
+    ):
+        found_user = await user_repository.find_by_id(
+            str(admin_user.id), str(admin_user.company_id)
+        )
+
+        assert isinstance(found_user, User)
+        assert found_user.id == admin_user.id
+        assert found_user.email == admin_user.email
+        assert found_user.name == admin_user.name
+        assert found_user.password == admin_user.password
+        assert found_user.role == admin_user.role
+        assert found_user.company_id == admin_user.company_id
+        assert found_user.avatar == admin_user.avatar
+        assert found_user.created_at == admin_user.created_at
+        assert found_user.updated_at == admin_user.updated_at
